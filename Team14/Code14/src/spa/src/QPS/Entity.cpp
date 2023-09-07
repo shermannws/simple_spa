806ed43653e901d0ad059af102281b90787da63b
lethiciars <santosolethicia@gmail.com>
2023-09-07 13:07:39 +0800

    add qps enitty implementation

--- Team14/Code14/src/spa/src/QPS/Entity.cpp
+++ Team14/Code14/src/spa/src/QPS/Entity.cpp
@@ -1,1 +1,16 @@
 #include "Entity.h"
+
+#include <string>
+
+Entity::Entity(EntityType type, const std::string& synonym) {
+    this->type = type;
+    this->synonym = synonym;
+}
+
+std::string Entity::getSynonym() {
+    return synonym;
+}
+
+EntityType Entity::getType() {
+    return type;
+}
--- Team14/Code14/src/spa/src/QPS/Entity.h
+++ Team14/Code14/src/spa/src/QPS/Entity.h
@@ -1,9 +1,30 @@
 #ifndef SPA_ENTITY_H
 #define SPA_ENTITY_H
 
+#include <string>
+
+enum class EntityType {
+    PROCEDURE,
+    STMT,
+    READ,
+    PRINT,
+    ASSIGN,
+    CALL,
+    WHILE,
+    IF,
+    VARIABLE,
+    CONSTANT,
+};
 
 class Entity {
-
+private:
+    std::string synonym;
+    EntityType type;
+public:
+    explicit Entity(EntityType type, const std::string &synonym);
+    std::string getSynonym();
+    EntityType getType();
+    //TODO toString() and eq(),neq() operator?
 };
 
 
