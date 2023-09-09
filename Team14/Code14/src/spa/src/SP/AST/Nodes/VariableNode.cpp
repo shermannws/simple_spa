fc76d468536c933506621687e24255cd951555d6
justinek <justinetaikoh@gmail.com>
2023-09-10 03:16:56 +0800

    Remove integer value from VariableNode definition

--- Team14/Code14/src/spa/src/SP/AST/Nodes/VariableNode.cpp
+++ Team14/Code14/src/spa/src/SP/AST/Nodes/VariableNode.cpp
@@ -1,6 +1,6 @@
 #include "VariableNode.h"
 
-VariableNode::VariableNode(std::string& varName, int value) : varName(varName), value(value) {}
+VariableNode::VariableNode(std::string& varName) : varName(varName) {}
 
 void VariableNode::accept(DesignExtractorVisitor& visitor) {
     visitor.visitVariableNode();
--- Team14/Code14/src/spa/src/SP/AST/Nodes/VariableNode.h
+++ Team14/Code14/src/spa/src/SP/AST/Nodes/VariableNode.h
@@ -7,10 +7,8 @@
 class VariableNode : public ExpressionNode {
 public:
     std::string varName;
-    // TODO: Wrap int type in typedef (VarValue)
-    int value;
 
-    explicit VariableNode(std::string& varName, int value);
+    explicit VariableNode(std::string& varName);
 
     void accept(DesignExtractorVisitor& visitor) override;
 };
