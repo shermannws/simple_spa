ca4a83b6c201783ded40cc7f12641f6d67088b8b
justinek <justinetaikoh@gmail.com>
2023-09-17 10:32:34 +0800

    Implement CALL, IF, WHILE classes

--- Team14/Code14/src/spa/src/SP/AST/Nodes/CallNode.h
+++ Team14/Code14/src/spa/src/SP/AST/Nodes/CallNode.h
@@ -14,7 +14,7 @@ class CallNode: public StatementNode {
 private:
     std::string procedureName;
 public:
-    explicit CallNode(std::string procedureName);
+    explicit CallNode(int statementNumber, std::string procedureName);
     std::string getProcedureName();
     void accept(std::shared_ptr<DesignExtractorVisitor> visitor) override;
     std::vector<std::shared_ptr<ASTNode>> getAllChildNodes() override;
