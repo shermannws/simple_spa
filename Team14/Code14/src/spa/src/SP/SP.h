1b3cb285a85dc7d235dbcfc9c862777cabbc07a1
justinek <justinetaikoh@gmail.com>
2023-09-22 01:43:12 +0800

    Add comments and use typedefs for SP and SPParser

--- Team14/Code14/src/spa/src/SP/SP.h
+++ Team14/Code14/src/spa/src/SP/SP.h
@@ -6,9 +6,22 @@
 
 class SP {
 private:
+    /**
+     * PkbWriter instance for writing to the PKB.
+     */
 	std::shared_ptr<PkbWriter> pkbWriter;
 
 public:
+    /**
+     * Creates and initiates an SP with a PkbWriter.
+     * @param pkbWriter PkbWriter instance for writing to the PKB.
+     */
 	SP(std::shared_ptr<PkbWriter> pkbWriter);
+
+    /**
+     * Initiates the tokenizing, validation and parsing of the source program.
+     * Initiates the extraction of relationships and population to PKB.
+     * @param input Source program to be processed
+     */
 	void startSPProcessing(std::string& input);
 };
--- Team14/Code14/src/spa/src/SP/SPParser.h
+++ Team14/Code14/src/spa/src/SP/SPParser.h
@@ -6,6 +6,7 @@
 #include <unordered_map>
 #include <functional>
 
+#include "Commons/AppConstants.h"
 #include "SPToken.h"
 #include "AST/Nodes/ASTNode.h"
 #include "AST/Nodes/ProgramNode.h"
@@ -29,40 +30,143 @@
 
 class SPParser {
 private:
-    int runningStatementNumber;
+    /**
+     * Running number used to label consecutive statements in the source program.
+     */
+    StatementNumber runningStatementNumber;
 
+    /**
+     * Consumes tokens, creates a ProcedureNode and returns a pointer to it.
+     * Calls the parser method for StatementList.
+     * @param tokens Queue of tokens passed by reference
+     * @return A pointer to the created ProcedureNode
+     */
     std::shared_ptr<ProcedureNode> parseProcedure(std::queue<SPToken>& tokens);
+
+    /**
+     * Consumes tokens, creates a StatementListNode and returns a pointer to it.
+     * Calls the correct parser methods for each statement in the StatementList.
+     * @param tokens Queue of tokens passed by reference
+     * @return A pointer to the created StatementListNode
+     */
     std::shared_ptr<StatementListNode> parseStatementList(std::queue<SPToken>& tokens);
+
+    /**
+     * Consumes tokens, creates an AssignNode and returns a pointer to it.
+     * Calls the parser methods for Variable and Expression.
+     * @param tokens Queue of tokens passed by reference
+     * @return A pointer to the created AssignNode
+     */
     std::shared_ptr<AssignNode> parseAssignStatement(std::queue<SPToken>& tokens);
+
+    /**
+     * Consumes tokens, creates a ReadNode and returns a pointer to it.
+     * Calls the parser method for Variable.
+     * @param tokens Queue of tokens passed by reference
+     * @return A pointer to the created ReadNode
+     */
     std::shared_ptr<ReadNode> parseReadStatement(std::queue<SPToken>& tokens);
+
+    /**
+     * Consumes tokens, creates a PrintNode and returns a pointer to it.
+     * Calls the parser method for Variable.
+     * @param tokens Queue of tokens passed by reference
+     * @return A pointer to the created PrintNode
+     */
     std::shared_ptr<PrintNode> parsePrintStatement(std::queue<SPToken>& tokens);
+
+    /**
+     * Consumes tokens, creates a CallNode and returns a pointer to it.
+     * @param tokens Queue of tokens passed by reference
+     * @return A pointer to the created CallNode
+     */
     std::shared_ptr<CallNode> parseCallStatement(std::queue<SPToken>& tokens);
+
+    /**
+     * Consumes tokens, creates an IfNode and returns a pointer to it.
+     * Calls the parser methods for ConditionalExpression and StatementList.
+     * @param tokens Queue of tokens passed by reference
+     * @return A pointer to the created IfNode
+     */
     std::shared_ptr<IfNode> parseIfStatement(std::queue<SPToken>& tokens);
+
+    /**
+     * Consumes tokens, creates a WhileNode and returns a pointer to it.
+     * Calls the parser methods for ConditionalExpression and StatementList.
+     * @param tokens Queue of tokens passed by reference
+     * @return A pointer to the created WhileNode
+     */
     std::shared_ptr<WhileNode> parseWhileStatement(std::queue<SPToken>& tokens);
+
+    /**
+     * Recursively creates and combines VariableNodes, ConstantNodes and/or ArithmeticExpressionNodes in pairs
+     * to form one final ExpressionNode.
+     * Calls the parser methods for Variable and Constant where necessary.
+     * @param tokens Queue of tokens passed by reference
+     * @param endDelimiter The TokenType of the token after the expression
+     * @return A pointer to the final ExpressionNode
+     */
     std::shared_ptr<ExpressionNode> parseExpression(std::queue<SPToken>& tokens, TokenType endDelimiter);
+
+    /**
+     * Consumes tokens, creates an VariableNode and returns a pointer to it.
+     * @param tokens Queue of tokens passed by reference
+     * @return A pointer to the created VariableNode
+     */
     std::shared_ptr<VariableNode> parseVariable(std::queue<SPToken>& tokens);
+
+    /**
+     * Consumes tokens, creates a ConstantNode and returns a pointer to it.
+     * @param tokens Queue of tokens passed by reference
+     * @return A pointer to the created ConstantNode
+     */
     std::shared_ptr<ConstantNode> parseConstant(std::queue<SPToken>& tokens);
+
+    /**
+     * Recursively creates and combines RelativeExpressionNodes, UnaryConditionalExpressionNodes and
+     * BinaryConditionalExpressionNodes in pairs to form one final ConditionalExpressionNode.
+     * Calls the parser methods for RelativeExpression.
+     * @param tokens Queue of tokens passed by reference
+     * @return A pointer to the final ConditionalExpressionNode
+     */
     std::shared_ptr<ConditionalExpressionNode> parseConditionalExpression(std::queue<SPToken>& tokens);
+
+    /**
+     * Consumes tokens, creates a RelativeExpressionNode and returns a pointer to it.
+     * Calls the parser methods for Expression.
+     * @param tokens Queue of tokens passed by reference
+     * @return A pointer to the created RelativeExpressionNode
+     */
     std::shared_ptr<RelativeExpressionNode> parseRelativeExpression(std::queue<SPToken>& tokens);
 
     /**
      * Returns an int representing the precedence value of the given operator,
-     * used for relative comparison with other operators
+     * used for relative comparison with other operators.
      * @param operatorToken Operator token to obtain precedence value
      * @return Int representing precedence value of given operator
      */
     static int getOperatorPrecedence(SPToken& operatorToken);
 
     /**
-     * Helper function for SPParser::parseExpression
+     * Helper function for SPParser::parseExpression.
+     * Converts a sequence of tokens representing an arithmetic expression in infix to postfix.
      * See shunting yard algorithm: https://en.wikipedia.org/wiki/Shunting_yard_algorithm
      * @param tokens Queue of tokens passed by reference
-     * @param endDelimiter TokenType of token after the last token to be converted to postfix
-     * @return Sorted queue of tokens
+     * @param endDelimiter The TokenType of the token after the arithmetic expression
+     * @return Sorted queue of tokens in postfix
      */
     static std::queue<SPToken> infixToPostfix(std::queue<SPToken>& tokens, TokenType endDelimiter);
+
 public:
+    /**
+     * Creates and initiates an SPParser.
+     */
     SPParser();
 
+    /**
+     * Parses the program tokens into an AST and returns a pointer to the root ProgramNode of the AST.
+     * @param tokens Tokens to be parsed
+     * @return A pointer to the root ProgramNode of the AST
+     */
     std::shared_ptr<ProgramNode> parse(std::vector<SPToken> tokens);
 };
