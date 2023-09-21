96d450199e7bbecacc4d32dd01c939f4a1c0e942
Jiwei99 <jwlim99@gmail.com>
2023-09-21 19:13:30 +0800

    Refactor Modifies and Uses Relationship to use StmtToVarRelationshipManager

--- Team14/Code14/src/spa/src/PKB/Managers/UsesRelationshipManager.cpp
+++ Team14/Code14/src/spa/src/PKB/Managers/UsesRelationshipManager.cpp
@@ -2,62 +2,32 @@
 
 #include "UsesRelationshipManager.h"
 
-UsesRelationshipManager::UsesRelationshipManager()
-    : usesRelationshipStore(std::make_shared<UsesRelationshipStore>(UsesRelationshipStore())) {}
+UsesRelationshipManager::UsesRelationshipManager() : StmtToVarRelationshipManager<UsesRelationshipStore, UsesRelationship>() {};
 
 void UsesRelationshipManager::storeUsesRelationship(std::shared_ptr<Statement> statement, std::shared_ptr<Variable> variable) {
-    this->usesRelationshipStore->storeRelationship(std::make_shared<UsesRelationship>(UsesRelationship(statement, variable)));
+    return this->storeRelationship(statement, variable);
 }
 
 std::vector<std::vector<Entity>> UsesRelationshipManager::getUsesStmtPair(StatementType type) const {
-    auto matcher = [type](UsesRelationship& usesRelationship) {
-        return usesRelationship.getLeftObject()->isStatementType(type);
-    };
-    return ManagerUtils::getEntityPairsFromStore<UsesRelationshipStore, UsesRelationship>(usesRelationshipStore,
-                                                                                         matcher,
-                                                                                         UsesRelationship::getEntityPairFromRelationship);
+    return this->getRelationshipStmtPair(type);
 }
 
 std::vector<Entity> UsesRelationshipManager::getUsesTypeIdent(StatementType type, Variable& var) const {
-    auto matcher = [type, var](UsesRelationship& usesRelationship) {
-        return *(usesRelationship.getRightObject()) == var && usesRelationship.getLeftObject()->isStatementType(type);
-    };
-    return ManagerUtils::getEntitiesFromStore<UsesRelationshipStore, UsesRelationship>(usesRelationshipStore,
-                                                                                         matcher,
-                                                                                         UsesRelationship::getLeftEntityFromRelationship);
+    return this->getRelationshipTypeIdent(type, var);
 }
 
 std::vector<Entity> UsesRelationshipManager::getUsesStmt(StatementType type) const {
-    auto set = std::unordered_set<Entity>();
-
-    auto matcher = [type, &set](UsesRelationship& usesRelationship) {
-        return usesRelationship.getLeftObject()->isStatementType(type) && set.find(*(usesRelationship.getLeftEntity())) == set.end();
-    };
-
-    auto getter = [&set](UsesRelationship& usesRelationship) {
-        set.insert(*(usesRelationship.getLeftEntity()));
-        return *(usesRelationship.getLeftEntity());
-    };
-    return ManagerUtils::getEntitiesFromStore<UsesRelationshipStore, UsesRelationship>(usesRelationshipStore,
-                                                                                         matcher,
-                                                                                         getter);
+    return this->getRelationshipStmt(type);
 }
 
 std::vector<Entity> UsesRelationshipManager::getUsesVar(Statement& stmt) const {
-    auto matcher = [&stmt](UsesRelationship& usesRelationship) {
-        return *(usesRelationship.getLeftObject()) == stmt;
-    };
-    return ManagerUtils::getEntitiesFromStore<UsesRelationshipStore, UsesRelationship>(usesRelationshipStore,
-                                                                                         matcher,
-                                                                                         UsesRelationship::getRightEntityFromRelationship);
+    return this->getRelationshipVar(stmt);
 }
 
 bool UsesRelationshipManager::isStmtUsesVar(Statement& stmt, Variable& var) const {
-    return usesRelationshipStore->getRelationship(std::make_shared<UsesRelationship>(std::make_shared<Statement>(stmt), std::make_shared<Variable>(var))) != nullptr;
+    return this->isRelationship(stmt, var);
 }
 
 bool UsesRelationshipManager::hasUses(Statement& stmt) const {
-    return std::find_if(usesRelationshipStore->getBeginIterator(), usesRelationshipStore->getEndIterator(), [&stmt](std::shared_ptr<UsesRelationship> relationship) {
-        return *relationship->getLeftEntity() == stmt;
-    }) != usesRelationshipStore->getEndIterator();
+    return this->hasRelationship(stmt);
 }
