f78d73edbb781d681a4739c879288a91a5f569ad
lethiciars <santosolethicia@gmail.com>
2023-09-23 07:41:33 +0800

    refactor result combiner to subcomponent

--- Team14/Code14/src/spa/src/QPS/PQLEvaluator.cpp
+++ Team14/Code14/src/spa/src/QPS/PQLEvaluator.cpp
@@ -8,7 +8,10 @@
 #include "FollowsSuchThatStrategy.h"
 #include "QPSTypes.h"
 
-PQLEvaluator::PQLEvaluator(std::shared_ptr<PkbReader> pkbReader) :pkbReader(pkbReader),clauseHandler(std::make_shared<ClauseHandler>(pkbReader)){}
+PQLEvaluator::PQLEvaluator(std::shared_ptr<PkbReader> pkbReader) :
+    pkbReader(pkbReader),
+    clauseHandler(std::make_shared<ClauseHandler>(pkbReader)),
+    resultHandler(std::make_shared<ResultHandler>()) {}
 
 ResultList PQLEvaluator::formatResult(Query& query, Result& result) {
     std::vector<EntityPtr> selects = query.getSelect();
@@ -67,10 +70,9 @@ Result PQLEvaluator::evaluate(Query& query) {
         }
     }
 
-    Result result = getCombined(sResult, pResult);
+    Result result = resultHandler->getCombined(sResult, pResult);
 
     // check if synonym in select is in result
-//     ASSUMES ONLY ONE SELECT VARIABLE RETURNED
     Synonym syn = query.getSelect()[0]->getSynonym();
     SynonymMap indicesMap = result.getSynIndices();
     if (indicesMap.find(syn) != indicesMap.end()) {
@@ -115,122 +117,4 @@ std::vector<Entity> PQLEvaluator::getAll(const std::shared_ptr<QueryEntity>& que
         default:
             throw std::runtime_error("Not supported entity type in query select clause");
     }
-}
-
-Result PQLEvaluator::getCombined(Result& r1, Result& r2) {
-    if (r1.getType() == ResultType::Invalid) {
-        return r2;
-    }
-    if (r2.getType() == ResultType::Invalid) {
-        return r1;
-    }
-    if (r1.getType() == ResultType::Boolean && r1.getBoolResult()) {
-        return r2;
-    }
-    if (r1.getType() == ResultType::Boolean && !r1.getBoolResult()) {
-        return r1;
-    }
-    if (r2.getType() == ResultType::Boolean && r2.getBoolResult()) {
-        return r1;
-    }
-    if (r2.getType() == ResultType::Boolean && !r2.getBoolResult()) {
-        return r2;
-    }
-
-    return join(r1, r2);
-}
-
-
-Result PQLEvaluator::join(Result& r1, Result& r2) {
-    Result final;
-    ResultTuples finalTuples;
-
-    // find common columns
-    std::unordered_map<int, int> common = getCommonColumns(r1, r2);
-    SynonymMap synIndices = buildSynIndices(r1, r2);
-    std::vector<Synonym> header = getHeader(synIndices);
-    final.setSynIndices(synIndices);
-
-    auto tuples1 = r1.getTuples();
-    auto tuples2 = r2.getTuples();
-
-    for (const auto& row1 : tuples1){
-        for (const auto& row2 : tuples2) {
-            if (isMatch(row1, row2, common)){
-                //build row and add to final's tuples
-                std::vector<Entity> newRow;
-                auto map1 = r1.getSynIndices();
-                auto map2 = r2.getSynIndices();
-                for (const auto& colName : header) {
-                    if (map1.count(colName)){ //if found in r1, add to vector
-                        int index = map1[colName];
-                        newRow.push_back(row1[index]);
-                        continue;
-                    }
-                    //add from r2
-                    int index = map2[colName];
-                    newRow.push_back(row2[index]);
-                }
-                finalTuples.push_back(newRow);
-            }
-        }
-    }
-    final.setTuples(finalTuples);
-    ResultType type = ResultType::Tuples;
-    final.setType(type);
-    return final;
-}
-
-
-// returns mapping of {index in r1: index in r2} for common cols
-std::unordered_map<int, int> PQLEvaluator::getCommonColumns(Result& r1, Result& r2) {
-    std::unordered_map<int, int> commonIndices;
-    auto map1 = r1.getSynIndices();
-    auto map2 = r2.getSynIndices();
-
-    for (auto& it: map1) {
-        Synonym key = it.first;
-        if (map2.count(key)) {
-            commonIndices[map1[key]] = map2[key];
-        }
-
-    }
-    return commonIndices;
-}
-
-// get combined synIndices
-SynonymMap PQLEvaluator::buildSynIndices(Result& r1, Result& r2) {
-    SynonymMap combined = r1.getSynIndices();
-    SynonymMap other = r2.getSynIndices();
-    int mapSize = combined.size();
-
-    for (auto& it: other) {
-        Synonym key = it.first;
-        if (combined.count(key) == 0) {
-           combined[it.first] = mapSize;
-           mapSize += 1;
-        }
-    }
-    return combined;
-}
-
-std::vector<Synonym> PQLEvaluator::getHeader(SynonymMap map) {
-    std::vector<Synonym> header(map.size());
-    for (auto& it: map) {
-        header[it.second] = it.first;
-    }
-    return header;
-}
-
-
-bool PQLEvaluator::isMatch(const std::vector<Entity>& row1,
-                           const std::vector<Entity>& row2,
-                           const std::unordered_map<int, int>& commons) {
-    for (auto& it: commons) {
-        if (row1[it.first] == row2[it.second]){
-            continue;
-        }
-        return false;
-    }
-    return true;
 }
--- Team14/Code14/src/spa/src/QPS/PQLEvaluator.h
+++ Team14/Code14/src/spa/src/QPS/PQLEvaluator.h
@@ -6,6 +6,7 @@
 #include "Result.h"
 #include "Query.h"
 #include "ClauseHandler.h"
+#include "ResultHandler.h"
 #include "./PKB/PkbReader.h"
 #include "QPSTypes.h"
 
@@ -13,17 +14,11 @@ class PQLEvaluator {
 private:
     std::shared_ptr<PkbReader> pkbReader;
     std::shared_ptr<ClauseHandler> clauseHandler;
+    std::shared_ptr<ResultHandler> resultHandler;
     std::vector<Entity> getAll(const EntityPtr& queryEntity);
 
 public:
     explicit PQLEvaluator(std::shared_ptr<PkbReader> pkbReader);
     Result evaluate(Query&);
     ResultList formatResult(Query& query, Result& result); // TODO: to set as private in the future
-    Result getCombined(Result& r1, Result& r2);
-
-    Result join(Result& r1, Result& r2);
-    std::unordered_map<int, int> getCommonColumns(Result& r1, Result& r2);
-    SynonymMap buildSynIndices(Result& r1, Result& r2);
-    std::vector<Synonym> getHeader(SynonymMap);
-    bool isMatch(const std::vector<Entity>& row1, const std::vector<Entity>& row2, const std::unordered_map<int, int>& commons);
 };
