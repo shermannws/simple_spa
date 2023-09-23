39c12088bb0492dd34c7c20408b22fe802f3bc64
lethiciars <santosolethicia@gmail.com>
2023-09-24 02:52:08 +0800

    add inline docs

--- Team14/Code14/src/spa/src/QPS/ExprSpecParser.h
+++ Team14/Code14/src/spa/src/QPS/ExprSpecParser.h
@@ -9,13 +9,49 @@
 
 class ExprSpecParser {
 private:
+    /**
+     * @brief The tokenizer that produces the stream of tokens
+     */
     std::shared_ptr<Tokenizer> tokenizer;
 
+    /**
+     * Returns true if the vector of tokens is a valid expr
+     * @param input vector of Token ptr
+     * @param index starting index of vector that needs to be validated
+     * @return boolean
+     */
     bool isExpr(std::vector<std::shared_ptr<Token>>& input, size_t& index);
+
+    /**
+     * Returns true if the vector of tokens is a valid term
+     * @param input vector of Token ptr
+     * @param index starting index of vector that needs to be validated
+     * @return boolean
+     */
     bool isTerm(std::vector<std::shared_ptr<Token>>& input, size_t& index);
+
+    /**
+     * Returns true if the vector of tokens is a valid factor
+     * @param input vector of Token ptr
+     * @param index starting index of vector that needs to be validated
+     * @return boolean
+     */
     bool isFactor(std::vector<std::shared_ptr<Token>>& input, size_t& index);
+
+    /**
+     * Returns true if the vector of tokens is a valid Expr
+     * @param input vector of Token ptr
+     * @return boolean
+     */
     bool validateExprSyntax(std::vector<std::shared_ptr<Token>>& input);
+
+    /**
+     * Returns true if the vector of tokens is a valid Expr
+     * @param input vector of Token ptr
+     * @return boolean
+     */
     void processSubExpr(std::stack<std::shared_ptr<Token>>& operators, std::stack<std::string>& expression);
+
     Expression extractExpression();
 
 public:
--- Team14/Code14/src/spa/src/QPS/ResultHandler.h
+++ Team14/Code14/src/spa/src/QPS/ResultHandler.h
@@ -6,16 +6,64 @@
 #include "Result.h"
 #include "Query.h"
 
+/**
+ * The ResultHandler class generates a result table from the combining two tables
+ * and handles result table join operations.
+ */
 class ResultHandler {
 private:
+    /**
+     * Performs a natural join on two result tables
+     * @param r1 first result table
+     * @param r2 second result table
+     * @return the resultant table from the join
+     */
     Result join(Result& r1, Result& r2);
+
+    /**
+     * Gets a map of indices of the common synonyms between two tables
+     * @param r1 first result table
+     * @param r2 second result table
+     * @return map of index of common synonyms in table 1 to index in table 2
+     */
     std::unordered_map<int, int> getCommonColumns(Result& r1, Result& r2);
+
+    /**
+     * Builds a map of synonym to index for the resultant combined table
+     * @param r1 first result table
+     * @param r2 second result table
+     * @return map of synonym to index
+     */
     SynonymMap buildSynIndices(Result& r1, Result& r2);
+
+    /**
+     * Creates a vector of the synonyms in the order of their index in map
+     * @param SynonymMap map of synonyms to index
+     * @return vector of the synonyms
+     */
     std::vector<Synonym> getHeader(SynonymMap);
+
+    /**
+     * Returns true if the two rows have the same value for the common synonyms (if any)
+     * @param row1 row from table 1
+     * @param row2 row from table 2
+     * @return boolean
+     */
     bool isMatch(const std::vector<Entity>& row1, const std::vector<Entity>& row2, const std::unordered_map<int, int>& commons);
 
 public:
+    /**
+     * Constructor for ResultHandler class
+     * @return A ResultHandler object
+     */
     explicit ResultHandler();
+
+    /**
+     * Evaluates the final result form combining 2 results
+     * @param r1 first result table
+     * @param r2 second result table
+     * @return the resultant table
+     */
     Result getCombined(Result& r1, Result& r2);
 };
 
