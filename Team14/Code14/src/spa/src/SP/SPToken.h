7a18aacf4253758af7dc7d9eaea8e16f88243e47
timothy <timothywongej@gmail.com>
2023-09-25 01:01:56 +0800

    Add docs to header files

--- Team14/Code14/src/spa/src/SP/SPToken.h
+++ Team14/Code14/src/spa/src/SP/SPToken.h
@@ -5,15 +5,43 @@
 
 #include "SPTokenType.h"
 
+/**
+ * SPTokens which are created by the SPTokenizer when it tokenizes the SIMPLE input source program.
+ */
 class SPToken {
-
 private:
+    /**
+     * The type of the SPToken.
+     */
     TokenType type;
+
+    /**
+     * The value of the SPToken stored as a string.
+     */
     std::string value;
 
 public:
+    /**
+     * Creates and initialises an SPToken.
+     * @param type Type of the token
+     * @param value Value of the token as a string
+     */
     SPToken(TokenType type, std::string  value);
+
+    /**
+     * Creates and initialises an empty SPToken.
+     */
     SPToken();
+
+    /**
+     * Returns the type of the SPToken.
+     * @return
+     */
     TokenType getType();
+
+    /**
+     * Returns the value of the SPToken.
+     * @return Value as string
+     */
     std::string getValue();
 };
