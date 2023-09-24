f3d057d7a56a8f873bf3762c11ddcb13b5c1a9ed
lethiciars <santosolethicia@gmail.com>
2023-09-24 19:22:29 +0800

    reorder functions

--- Team14/Code14/src/spa/src/QPS/PQLParser.h
+++ Team14/Code14/src/spa/src/QPS/PQLParser.h
@@ -18,16 +18,19 @@ private:
     Synonym processSelectClause(Query& query);
     std::shared_ptr<SuchThatClause> processSuchThatClause(Query& query);
     std::shared_ptr<PatternClause> processPatternClause(Query& query);
+
+    void validateSuchThatSyntax(std::shared_ptr<SuchThatClause> clause);
+    void validateSuchThatRefType(const std::shared_ptr<SuchThatClause> clause);
+    void validatePatternSyntax(std::shared_ptr<PatternClause> clause);
+
+    void validateDeclarations(Query& query, const std::vector<std::shared_ptr<QueryEntity>>& entities);
+    void validateSelectSemantics(Query& query, const Synonym& syn);
+    void validateSuchThatSemantics(Query& query, const std::shared_ptr<SuchThatClause> clause);
+    void validatePatternSemantics(Query& query, const std::shared_ptr<PatternClause> clause);
+
     std::shared_ptr<Token> expect(bool isToken, const std::string& errorMsg);
     Ref extractRef();
     std::shared_ptr<QueryEntity> extractQueryEntity(std::shared_ptr<Token> entityType);
-    void validateDeclarations(Query& query, const std::vector<std::shared_ptr<QueryEntity>>& entities);
-    void validateSuchThatSyntax(std::shared_ptr<SuchThatClause> clause);
-    void validateSuchThatSemantics(Query& query, const std::shared_ptr<SuchThatClause> clause);
-    void validateSelectSemantics(Query& query, const Synonym& syn);
-    void validateSuchThatRefType(const std::shared_ptr<SuchThatClause> clause);
-    void validatePatternSyntax(std::shared_ptr<PatternClause> clause);
-    void validatePatternSemantics(Query& query, const std::shared_ptr<PatternClause> clause);
 
 public:
     explicit PQLParser(const std::string& PQLQuery);
