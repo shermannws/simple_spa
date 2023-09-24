e53f2585d7c08c46873f5a7449515d8863ca1601
Natania Yovela Purwanto <natania.yovela@gmail.com>
2023-09-24 16:36:58 +0800

    Add inline docs for Such That Strategies, Ref, Token, Tokenizer, QPSUtil and PQLParser

--- Team14/Code14/src/spa/src/QPS/Strategy.h
+++ Team14/Code14/src/spa/src/QPS/Strategy.h
@@ -10,18 +10,33 @@
  */
 class Strategy {
 protected:
-    // TODO: move this to util or make it static
+    /*
+     * The statement map that converts from QueryEntityType to the corresponding StatementType
+     */
     std::unordered_map<QueryEntityType, StatementType> stmtMap{
             {QueryEntityType::Assign, StatementType::Assign},
             {QueryEntityType::Print, StatementType::Print},
             {QueryEntityType::Read, StatementType::Read},
             {QueryEntityType::If, StatementType::If},
             {QueryEntityType::While, StatementType::While},
-//            {QueryEntityType::Call, StatementType::Call},
             {QueryEntityType::Stmt, StatementType::Stmt}
     };
 public:
+    /*
+     * @brief The constructor of Strategy
+     */
     Strategy() = default;
+
+    /*
+     * @brief The destructor of Strategy
+     */
     virtual ~Strategy() = default;
+
+    /*
+     * @brief Evaluates the clause using the appropriate pkbReader method
+     * @param clause The shared pointer of the Clause to be evaluated
+     * @param pkbReader The pkbReader of which the methods are to be called
+     * @return The result of the clause evaluation
+     */
     virtual Result evaluateClause(std::shared_ptr<Clause> clause, std::shared_ptr<PkbReader> pkbReader) const = 0;
 };
