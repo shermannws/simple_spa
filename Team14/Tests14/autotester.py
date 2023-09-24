f8f979e9e0dfdde5fc34479bf2d61170dbefd24a
Jiwei99 <jwlim99@gmail.com>
2023-09-25 00:03:18 +0800

    Add CI/CD to verify invalid sources

--- Team14/Tests14/autotester.py
+++ Team14/Tests14/autotester.py
@@ -4,7 +4,8 @@ from collections import Counter
 QUERY_TAG = "<query>"
 SUCCESS_TAG = "<passed/>"
 FAILURE_TAG = "</failed>"
-SUCCESS_EXIT_CODE = 0
+EXIT_SUCCESS = 0
+EXIT_FAILURE = 1
 TEST_SETTINGS = {
     "milestone1": True,
     "milestone2": False,
@@ -13,9 +14,10 @@ TEST_SETTINGS = {
 TEST_DIR = "./Tests14"
 AUTOTESTER_PATH = "./Code14/build/src/autotester/autotester"
 OUTPUT_PATH = "./Tests14/out.xml"
+EMPTY_FILE = "./Tests14/empty.txt"
 
 
-def validate_files(path, source_files, queries_files, keys):
+def validate_files(path, source_files, queries_files, error_files, valid_keys, error_keys):
     missing_files = []
     for dir_path, directories, filenames in os.walk(path):
         prefix = dir_path.split(path, 1)[1]
@@ -26,31 +28,40 @@ def validate_files(path, source_files, queries_files, keys):
             elif filename.endswith("_queries.txt"):
                 queries_key = prefix + "/" + filename.rsplit("_queries.txt", 1)[0]
                 queries_files[queries_key] = os.path.join(dir_path, filename)
+            elif filename.endswith("_error.txt"):
+                error_key = prefix + "/" + filename.rsplit("_error.txt", 1)[0]
+                error_files[error_key] = os.path.join(dir_path, filename)
 
         for key in source_files.keys():
             if key not in queries_files:
                 missing_files.append(f"{key}_queries.txt")
             else:
-                keys.add(key)
+                valid_keys.add(key)
 
         for key in queries_files.keys():
             if key not in source_files:
                 missing_files.append(f"{key}_source.txt")
             else:
-                keys.add(key)
+                valid_keys.add(key)
+
+        for key in error_files.keys():
+            error_keys.add(key)
 
     if len(missing_files) > 0:
         raise Exception("Missing files: " + ", ".join(missing_files))
     else:
-        print(f"Found {len(keys)} sets of System Tests")
+        print(f"Found {len(valid_keys) + len(error_keys)} sets of System Tests")
 
 
-def run_tests(source_files, queries_files, keys, failures):
-    for key in keys:
-        print(f"Running System Test {key}...")
+def run_tests(source_files, queries_files, valid_keys, error_files, error_keys, failures):
+    for key in valid_keys:
+        print(f"Running Valid System Test {key}...")
         execute(key, source_files[key], queries_files[key])
         validate_output(key, failures)
         clean_up()
+    for key in error_keys:
+        print(f"Running Erroneous System Test {key}...")
+        execute_with_error(key, error_files[key], failures)
 
 
 def execute(key, source_filepath, query_filepath):
@@ -65,11 +76,28 @@ def execute(key, source_filepath, query_filepath):
 
     exit_code = os.system(command)
 
-    if exit_code != SUCCESS_EXIT_CODE:
+    if exit_code != EXIT_SUCCESS:
         raise Exception(
             f"Autotester execution failed for {key} with exit code: {exit_code}. Please run this test manually to verify.")
     else:
-        print(f"Autotester execution successful for {key}")
+        print(f"Success: Autotester execution successful for {key}")
+
+
+def execute_with_error(key, filepath, failures_list):
+    if not os.path.exists(filepath):
+        raise Exception(f"Missing path {filepath}")
+
+    command = f'{AUTOTESTER_PATH} {filepath} {EMPTY_FILE} {OUTPUT_PATH}'
+
+    command = command + "> /dev/null"
+
+    exit_code = os.system(command)
+
+    if os.WEXITSTATUS(exit_code) == EXIT_FAILURE:
+        print(f"Success: Autotester execution failed for {key} as expected")
+    else:
+        raise Exception(
+            f"Autotester execution failed for {key} with exit code: {exit_code} instead of exit code 1. Test is not failing as expected. Please run this test manually to verify.")
 
 
 def validate_output(key, failures_list):
@@ -86,7 +114,7 @@ def validate_output(key, failures_list):
         print(f"Total queries: {total}; Successful queries: {successes}; Failed queries: {failures}")
 
         if successes == total:
-            print(f"All queries passed for {key}!")
+            print(f"Success: All queries passed for {key}!")
         else:
             failures_list.append(key)
             print(f"ERROR: Failing tests for {key}")
@@ -103,16 +131,18 @@ def run():
 
             source_files = {}
             queries_files = {}
-            keys = set()
+            error_files = {}
+            valid_keys = set()
+            error_keys = set()
             failures = []
 
-            validate_files(os.path.join(TEST_DIR, test_set), source_files, queries_files, keys)
-            run_tests(source_files, queries_files, keys, failures)
+            validate_files(os.path.join(TEST_DIR, test_set), source_files, queries_files, error_files, valid_keys, error_keys)
+            run_tests(source_files, queries_files, valid_keys, error_files, error_keys, failures)
 
             if len(failures) > 0:
                 raise Exception(f"System Tests failed for {len(failures)} sets in {test_set}: " + ", ".join(failures))
             else:
-                print(f"All System Tests passed for {test_set}!\n")
+                print(f"Success! All System Tests passed for {test_set}!\n")
 
 
 if __name__ == "__main__":
