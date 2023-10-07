b158d15d3e8846a583e01dae3c184264f1a454dc
timothy <timothywongej@gmail.com>
2023-10-07 22:39:37 +0800

    Add procedure to statement mappings in CallsExtractorVisitor

--- Team14/Code14/src/spa/src/SP/AST/Visitors/CallsExtractorVisitor.cpp
+++ Team14/Code14/src/spa/src/SP/AST/Visitors/CallsExtractorVisitor.cpp
@@ -1,4 +1,5 @@
 #include "CallsExtractorVisitor.h"
+#include "Commons/StatementTypeFactory.h"
 
 CallsExtractorVisitor::CallsExtractorVisitor(std::shared_ptr<PkbWriter> pkbWriter) {
     this->pkbWriter = pkbWriter;
@@ -10,9 +11,32 @@ void CallsExtractorVisitor::visitCallNode(CallNode *node, std::vector<std::share
     std::shared_ptr<ProcedureNode> callerNode = std::dynamic_pointer_cast<ProcedureNode>(proc);
     Procedure caller = Procedure(callerNode->getProcedureName());
     Procedure callee = Procedure(node->getProcedureName());
+    std::shared_ptr<Procedure> callerPtr = std::make_shared<Procedure>(caller);
+    std::shared_ptr<Procedure> calleePtr = std::make_shared<Procedure>(callee);
 
     this->pkbWriter->addCallsRelationship(
-            std::make_shared<Procedure>(caller),
-            std::make_shared<Procedure>(callee)
+            callerPtr,
+            calleePtr
     );
+
+    // Add procedure to statement mapping
+    std::vector<std::shared_ptr<Statement>> statementsToAdd;
+    Statement currStatement = Statement(
+            node->getStatementNumber(),
+            StatementTypeFactory::getStatementTypeFrom(node->getStatementType())
+            );
+    statementsToAdd.push_back(std::make_shared<Statement>(currStatement));
+
+    // Add procedure to parent statements mapping for UsesP and ModifiesP
+    for (auto parent = parents.rbegin(); parent != parents.rend(); parent++) {
+        auto parentPtr = std::dynamic_pointer_cast<StatementNode>(*parent);
+        assert(parentPtr != nullptr);
+        Statement parentStatement = Statement(
+                parentPtr->getStatementNumber(),
+                StatementTypeFactory::getStatementTypeFrom(parentPtr->getStatementType())
+                );
+        statementsToAdd.push_back(std::make_shared<Statement>(parentStatement));
+    }
+
+    this->pkbWriter->addProcedureToStatementsMap(calleePtr, statementsToAdd);
 }
