6cf9549e81824b21b54d550c21f5b691c728d842
lethiciars <santosolethicia@gmail.com>
2023-10-08 14:12:22 +0800

    implement Calls strategy funcs

--- Team14/Code14/src/spa/src/QPS/Evaluators/Strategies/CallsSuchThatStrategy.cpp
+++ Team14/Code14/src/spa/src/QPS/Evaluators/Strategies/CallsSuchThatStrategy.cpp
@@ -11,17 +11,45 @@ std::shared_ptr<Result> CallsSuchThatStrategy::evaluateSynSyn(Ref &leftRef, Ref 
 }
 
 std::shared_ptr<Result> CallsSuchThatStrategy::evaluateSynAny(Ref &leftRef, Ref &rightRef) const {
-    // rhs = procName, wildcard
+    std::shared_ptr<Result> res = std::make_shared<Result>();
+    std::unordered_map<std::string, int> indices {{leftRef.getRep(), 0}};
+    res->setSynIndices(indices);
+    if (rightRef.isRootType(RootType::Wildcard)) {
+        //res->setTuples(pkbReader->getCallers());
+    }
+    if (rightRef.isRootType(RootType::Ident)) {
+        //res->setTuples(pkbReader->getCallers(rightRef.getRep()));
+    }
+    return res;
 }
 
 std::shared_ptr<Result> CallsSuchThatStrategy::evaluateAnySyn(Ref &leftRef, Ref &rightRef) const {
-    //lhs = procName, wildcard
+    std::shared_ptr<Result> res = std::make_shared<Result>();
+    std::unordered_map<std::string, int> indices {{rightRef.getRep(), 0}};
+    res->setSynIndices(indices);
+    if (leftRef.isRootType(RootType::Wildcard)) {
+        //res->setTuples(pkbReader->getCallees());
+    }
+    if (leftRef.isRootType(RootType::Ident)) {
+        //res->setTuples(pkbReader->getCallees(rightRef.getRep()));
+    }
+    return res;
 }
 
 std::shared_ptr<Result> CallsSuchThatStrategy::evaluateBoolean(Ref &leftRef, Ref &rightRef) const {
-    // both wildcard
-    // both non-wc
-    // RHS non-wc
-    // LHS non-wc
-
+    std::shared_ptr<Result> res = std::make_shared<Result>();
+    bool isLeftIdent = leftRef.isRootType(RootType::Ident);
+    bool isRightIdent = rightRef.isRootType(RootType::Ident);
+    if (isLeftIdent && isRightIdent) {
+        //res->setBoolResult(pkbReader->Calls(leftRef.getRep(), rightRef.getRep()));
+    }
+    else if (isLeftIdent) {
+        //res->setBoolResult(pkbReader->isCaller(leftRef.getRep()));
+    }
+    else if (isRightIdent) {
+        //res->setBoolResult(pkbReader->isCallee(rightRef.getRep()));
+    } else {
+        //res->setBoolResult(pkbReader->hasCalls());
+    }
+    return res;
 }
