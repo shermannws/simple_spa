229735e89fd5c9679b40d7b4f0ec5fbe9eb4c316
justinek <justinetaikoh@gmail.com>
2023-10-08 15:40:44 +0800

    Rename constants

--- Team14/Code14/src/spa/src/Commons/AppConstants.h
+++ Team14/Code14/src/spa/src/Commons/AppConstants.h
@@ -87,6 +87,6 @@ public:
             STRING_MINUS
     };
 
-    inline static const StatementNumber STARTING_STATEMENT_NUMBER = 1;
+    inline static const StatementNumber INITIAL_STATEMENT_NUMBER = 1;
     inline static const StatementNumber DUMMY_CFGNODE_STATEMENT_NUMBER = -1;
 };
