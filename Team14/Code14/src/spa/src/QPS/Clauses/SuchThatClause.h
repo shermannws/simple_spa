8148896aa68818d4301fbb44b4c0373b065448f8
lethiciars <santosolethicia@gmail.com>
2023-10-08 14:23:30 +0800

    add docs

--- Team14/Code14/src/spa/src/QPS/Clauses/SuchThatClause.h
+++ Team14/Code14/src/spa/src/QPS/Clauses/SuchThatClause.h
@@ -40,5 +40,10 @@ public:
      */
     Ref& getSecondParam();
 
+    /**
+     * @brief Returns true if the Clause object is equal to the other Clause object, false otherwise
+     * @param other The other Clause object to compare against
+     * @return True if the Clause object is equal to the other Clause object
+     */
     bool operator==(const Clause& other) const override;
 };
