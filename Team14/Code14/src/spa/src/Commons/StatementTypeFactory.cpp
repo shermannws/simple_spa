324a6da1d073da308c1d1ac1498d433eef791c9b
shermannws <shermannws@gmail.com>
2023-10-08 13:55:13 +0800

    Fix function pointer map in StatementTypeFactory

--- Team14/Code14/src/spa/src/Commons/StatementTypeFactory.cpp
+++ Team14/Code14/src/spa/src/Commons/StatementTypeFactory.cpp
@@ -2,12 +2,12 @@
 
 
 std::unordered_map<StatementNodeType, std::function<StatementType(void)>> StatementTypeFactory::functionMap = {
-	{StatementNodeType::Assign, [&]() -> StatementType { return StatementType::Assign; }},
-	{StatementNodeType::Print, [&]() -> StatementType { return StatementType::Print; }},
-	{StatementNodeType::Read, [&]() -> StatementType { return StatementType::Read; }},
-	{StatementNodeType::Call, [&]() -> StatementType { return StatementType::Call; }},
-	{StatementNodeType::While, [&]() -> StatementType { return StatementType::While; }},
-	{StatementNodeType::If, [&]() -> StatementType { return StatementType::If; }}
+	{StatementNodeType::Assign, []() -> StatementType { return StatementType::Assign; }},
+	{StatementNodeType::Print, []() -> StatementType { return StatementType::Print; }},
+	{StatementNodeType::Read, []() -> StatementType { return StatementType::Read; }},
+	{StatementNodeType::Call, []() -> StatementType { return StatementType::Call; }},
+	{StatementNodeType::While, []() -> StatementType { return StatementType::While; }},
+	{StatementNodeType::If, []() -> StatementType { return StatementType::If; }}
 };
 
 StatementType StatementTypeFactory::getStatementTypeFrom(StatementNodeType type) {
