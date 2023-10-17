4c1b98066d390419b6e51a15ce5d86ee5140937f
justinek <justinetaikoh@gmail.com>
2023-10-17 19:45:42 +0800

    Modify CallsExtractorVisitor

--- Team14/Code14/src/spa/src/SP/AST/Visitors/CallsExtractorVisitor.cpp
+++ Team14/Code14/src/spa/src/SP/AST/Visitors/CallsExtractorVisitor.cpp
@@ -1,5 +1,8 @@
 #include "CallsExtractorVisitor.h"
 #include "Commons/StatementTypeFactory.h"
+#include "Commons/Entities/CallStatement.h"
+#include "Commons/Entities/IfStatement.h"
+#include "Commons/Entities/WhileStatement.h"
 
 CallsExtractorVisitor::CallsExtractorVisitor(std::shared_ptr<PkbWriter> pkbWriter) {
     this->pkbWriter = pkbWriter;
@@ -8,7 +11,7 @@ CallsExtractorVisitor::CallsExtractorVisitor(std::shared_ptr<PkbWriter> pkbWrite
 void CallsExtractorVisitor::visitCallNode(CallNode *node, std::vector<std::shared_ptr<ASTNode>> parents,
                                           std::shared_ptr<ASTNode> proc) const {
 
-    std::shared_ptr<ProcedureNode> callerNode = std::dynamic_pointer_cast<ProcedureNode>(proc);
+    std::shared_ptr<ProcedureNode> callerNode = std::static_pointer_cast<ProcedureNode>(proc);
     Procedure caller = Procedure(callerNode->getProcedureName());
     Procedure callee = Procedure(node->getProcedureName());
     std::shared_ptr<Procedure> callerPtr = std::make_shared<Procedure>(caller);
@@ -21,21 +24,22 @@ void CallsExtractorVisitor::visitCallNode(CallNode *node, std::vector<std::share
 
     // Add procedure to statement mapping
     std::vector<std::shared_ptr<Statement>> statementsToAdd;
-    Statement currStatement = Statement(
-            node->getStatementNumber(),
-            StatementTypeFactory::getStatementTypeFrom(node->getStatementType())
-            );
-    statementsToAdd.push_back(std::make_shared<Statement>(currStatement));
+    CallStatement currStatement(node->getStatementNumber(), node->getProcedureName());
+    statementsToAdd.push_back(std::make_shared<CallStatement>(currStatement));
 
     // Add procedure to parent statements mapping for UsesP and ModifiesP
     for (auto parent = parents.rbegin(); parent != parents.rend(); parent++) {
-        auto parentPtr = std::dynamic_pointer_cast<StatementNode>(*parent);
+        auto parentPtr = std::static_pointer_cast<StatementNode>(*parent);
         assert(parentPtr != nullptr);
-        Statement parentStatement = Statement(
-                parentPtr->getStatementNumber(),
-                StatementTypeFactory::getStatementTypeFrom(parentPtr->getStatementType())
-                );
-        statementsToAdd.push_back(std::make_shared<Statement>(parentStatement));
+        StatementType statementType = StatementTypeFactory::getStatementTypeFrom(parentPtr->getStatementType());
+        if (statementType == StatementType::If) {
+            IfStatement parentStatement(parentPtr->getStatementNumber());
+            statementsToAdd.push_back(std::make_shared<IfStatement>(parentStatement));
+        } else {
+            assert(statementType == StatementType::While);
+            WhileStatement parentStatement(parentPtr->getStatementNumber());
+            statementsToAdd.push_back(std::make_shared<WhileStatement>(parentStatement));
+        }
     }
 
     this->pkbWriter->addProcedureToStatementsMap(calleePtr, statementsToAdd);
