96de4202d27394d3f0fca75fa6f8b20f4e7e8804
justinek <justinetaikoh@gmail.com>
2023-10-19 00:17:52 +0800

    Add documentation for CFGBuilder and CFGExtractor

--- Team14/Code14/src/spa/src/SP/CFG/CFGBuilder.h
+++ Team14/Code14/src/spa/src/SP/CFG/CFGBuilder.h
@@ -16,14 +16,14 @@ private:
     /**
      * Builds a CFG for the procedure represented by the subtree rooted at the ProcedureNode given.
      * @param procedureNode The root (ASTNode) of the procedure subtree to create a CFG for
-     * @return Unordered map of Statement to CFGNode
+     * @return A pair of (1) head CFGNode of the procedure, and (2) a vector of all CFGNodes of the procedure
      */
     static std::pair<std::shared_ptr<CFGNode>, std::vector<std::shared_ptr<CFGNode>>>
     buildCFGForProcedure(const std::shared_ptr<ProcedureNode> &procedureNode);
 
     /**
      * Builds a CFG subgraph for a statement list.
-     * @param map Unordered map of each procedure to a map of Statement to CFGNode
+     * @param cfgNodes A vector to hold all CFGNodes of the procedure
      * @param statementListNode The root (ASTNode) of the statement list subtree to create a CFG subtree for
      * @return The head and tail nodes of this subgraph
      */
@@ -33,7 +33,7 @@ private:
 
     /**
      * Builds a CFG subgraph for a statement (assign, call, print, read, if, while).
-     * @param map Unordered map of each procedure to a map of Statement to CFGNode
+     * @param cfgNodes A vector to hold all CFGNodes of the procedure
      * @param statementNode The root (ASTNode) of the statement subtree to create a CFG subtree for
      * @return The head and tail nodes of this subgraph
      */
@@ -43,7 +43,7 @@ private:
 
     /**
      * Builds a CFG subgraph for an if statement and its statement lists.
-     * @param map Unordered map of each procedure to a map of Statement to CFGNode
+     * @param cfgNodes A vector to hold all CFGNodes of the procedure
      * @param ifNode The root (ASTNode) of the if statement subtree to create a CFG subtree for
      * @return The head and tail nodes of this subgraph
      */
@@ -52,7 +52,7 @@ private:
 
     /**
      * Builds a CFG subgraph for a while statement and its statement list.
-     * @param map Unordered map of each procedure to a map of Statement to CFGNode
+     * @param cfgNodes A vector to hold all CFGNodes of the procedure
      * @param whileNode The root (ASTNode) of the while statement subtree to create a CFG subtree for
      * @return The head and tail nodes of this subgraph
      */
@@ -62,13 +62,10 @@ private:
 public:
     /**
      * Builds a CFG for each procedure in the AST given.
-     * Returns a pair of 2 maps required to traverse CFGs.
-     * 1. Unordered map of each procedure to the first Statement in the procedure, i.e. heads of each CFG, and
-     * 2. Unordered map of each procedure to an unordered map of Statement to CFGNode
      * @param ASTRootNode The root node of the AST
-     * @return A pair of:
-     * (1) Unordered map of ProcedureName to Statement, and
-     * (2) Unordered map of ProcedureName to an unordered map of Statement to CFGNode
+     * @return An unordered map of ProcedureName to a pair of:
+     * (1) head CFGNode of the procedure, and
+     * (2) a vector of all CFGNodes of the procedure
      */
     static std::unordered_map<ProcedureName, std::pair<std::shared_ptr<CFGNode>, std::vector<std::shared_ptr<CFGNode>>>>
     buildAllCFG(const std::shared_ptr<ProgramNode> &ASTRootNode);
--- Team14/Code14/src/spa/src/SP/CFG/CFGExtractor.h
+++ Team14/Code14/src/spa/src/SP/CFG/CFGExtractor.h
@@ -17,10 +17,19 @@ private:
      */
     std::shared_ptr<PkbWriter> pkbWriter;
 
+    /**
+     * Saves the CFG to the PKB using the PkbWriter.
+     * @param cfgMap An unordered map mapping each procedure to its head and all its CFGNodes
+     */
     void
     saveCFGToPKB(const std::unordered_map<
                  ProcedureName, std::pair<std::shared_ptr<CFGNode>, std::vector<std::shared_ptr<CFGNode>>>> &cfgMap);
 
+    /**
+     * Extracts the direct Next relationships from a CFG and saves them to the PKB using the PkbWriter.
+     * @param head The head node of the CFG
+     * @param cfgNodes A vector of CFGNodes of the CFG
+     */
     void extractNextRelationship(const std::shared_ptr<CFGNode> &head,
                                  const std::vector<std::shared_ptr<CFGNode>> &cfgNodes);
 
@@ -31,6 +40,10 @@ public:
      */
     explicit CFGExtractor(std::shared_ptr<PkbWriter> pkbWriter);
 
+    /**
+     * Initiates the saving of CFG to PKB, and the extraction of direct Next relationships for each CFG.
+     * @param cfgMap An unordered map mapping each procedure to its head and all its CFGNodes
+     */
     void extractRelationships(
             const std::unordered_map<
                     ProcedureName, std::pair<std::shared_ptr<CFGNode>, std::vector<std::shared_ptr<CFGNode>>>> &cfgMap);
