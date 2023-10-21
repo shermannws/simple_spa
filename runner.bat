5d19a77f0f22a4d457d1f8f9435c4898055f93b5
shermannws <shermannws@gmail.com>
2023-10-21 19:55:56 +0800

    Fix typo in with-cl sys test

---
+++ runner.bat
@@ -0,0 +1,5 @@
+@echo off
+.\Team14\Code14\out\build\x64-Debug\src\autotester\autotester.exe ^
+.\Team14\Tests14\%1\%2\%3_source.txt ^
+.\Team14\Tests14\%1\%2\%3_queries.txt ^
+.\Team14\Tests14\%1-%2-%3.xml
