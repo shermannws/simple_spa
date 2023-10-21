702d9ddef3f48adabfc56757553f5310546438b7
shermannws <shermannws@gmail.com>
2023-10-22 00:43:17 +0800

    Remove local test file

--- runner.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-.\Team14\Code14\out\build\x64-Debug\src\autotester\autotester.exe ^
-.\Team14\Tests14\%1\%2\%3_source.txt ^
-.\Team14\Tests14\%1\%2\%3_queries.txt ^
-.\Team14\Tests14\%1-%2-%3.xml
