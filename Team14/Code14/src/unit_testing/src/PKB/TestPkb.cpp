223891bb9249c83d61bdb1cbf97969435cb66088
shermannws <shermannws@gmail.com>
2023-10-25 13:06:25 +0800

    Fix unit test files TestPkb and TestPkbReader

--- Team14/Code14/src/unit_testing/src/PKB/TestPkb.cpp
+++ Team14/Code14/src/unit_testing/src/PKB/TestPkb.cpp
@@ -23,17 +23,17 @@ TEST_CASE("Test PKB end-to-end using Facades") {
     auto isFollows = reader->isFollows(statement2, statement3);
     REQUIRE(isFollows);
     auto followingStmts = reader->getFollowsStmtType(statement2, StatementType::Print);
-    REQUIRE(followingStmts.at(0) == Statement(3, StatementType::Print));
+    REQUIRE(followingStmts.find(Statement(3, StatementType::Print)) != followingStmts.end());
 
     writer->addConstant(std::make_shared<Constant>("2"));
     auto c = reader->getAllConstants();
-    REQUIRE(c.at(0) == Constant("2"));
+    REQUIRE(c.find(Constant("2")) != c.end());
 
     writer->addPrintStatement(std::make_shared<Statement>(5, StatementType::Print));
     auto pn = reader->getAllStatements();
-    REQUIRE(pn.at(0) == Statement(5, StatementType::Print));
+    REQUIRE(pn.find(Statement(5, StatementType::Print)) != pn.end());
 
     writer->addVariable(std::make_shared<Variable>("x"));
     auto v = reader->getAllVariables();
-    REQUIRE(v.at(0) == Variable("x"));
+    REQUIRE(v.find(Variable("x")) != v.end());
 }
